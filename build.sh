8ded337d0807538f43d4107d03c77ebe2cffb0e4
Matt Fellows <matt.fellows@onegeek.com.au>
2015-03-15 00:31:13 +1100

    Nuget restore should be first

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+nuget restore -NonInteractive
+
 PACKAGES_DIR=$(find ./packages/ -name *.dll)
 PACKAGES=""
 
@@ -7,6 +9,5 @@ for f in $PACKAGES_DIR; do
    PACKAGES="$PACKAGES $f"
 done
 
-nuget restore -NonInteractive
 xbuild /p:Configuration=Release ./console.sln
 mkbundle --deps --static ./obj/x86/Release/console.exe $PACKAGES -o consoleapp
